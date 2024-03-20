c6120de5218c4a90fc0d3703fd668a498319f015
rhubarb414 <ctophr.dubois@gmail.com>
2024-03-20 16:06:34 -0700

    Adds comment model

--- app/models/Post.py
+++ app/models/Post.py
@@ -13,3 +13,5 @@ class Post(Base):
     updated_at = Column(DateTime, default = datetime.now, onupdate = datetime.now)
 
     user = relationship('User')
+    comments = relationship('Comment', cascade='all,delete')
+
