7c73aeec960e5faa6d3d8dd8a032c11b88400b2a
rhubarb414 <ctophr.dubois@gmail.com>
2024-03-23 13:30:59 -0700

    adds sessions

--- app/routes/api.py
+++ app/routes/api.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, request, jsonify, session
 from app.models import User
 from app.db import get_db
+import sys
 
 
 bp = Blueprint('api', __name__, url_prefix='/api')
@@ -22,6 +23,7 @@ def signup():
         db.add(newUser)
         db.commit()
     except:
+        print(sys.exc_info()[0])
         # insert failed, so rollback send error to front end
         db.rollback()
         return jsonify(message = 'Signup failed'), 500
@@ -37,3 +39,24 @@ def logout():
     # remove session variables
     session.clear()
     return '', 204
+
+@bp.route('/users/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    db = get_db()
+
+    try:
+        user = db.query(User).filter(User.email == data['email']).one()
+    except:
+        print(sys.exc_info()[0])
+
+        return jsonify(message = 'Incorrect credentials'), 400
+    
+    if user.verify_password(data['password']) == False:
+        return jsonify(message = 'Incorrect credentials'), 400
+    
+    session.clear()
+    session['user_id'] = user.id
+    session['loggedIn'] = True
+
+    return jsonify(id = user.id)
